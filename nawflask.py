5d19d3d035a9e8b95074712f77d2bd6018426e2b
nawfalkaysan <nawfalkasyanre@gmail.com>
2025-02-16 18:35:43 +0700

    Penambahan POST,GET
    kita bisa menerima data dari post yg kemudian bisa diproseses dan dikembalikan

--- nawflask.py
+++ nawflask.py
@@ -12,6 +12,17 @@ def dimana():
     return jsonify(message="dirumah lah kocak")
 
 
+@app.route('/tanya',methods =['POST','GET'])
+def tanya():
+    if request.method == 'POST':
+        body = request.get_json()
+        print(body['lu suka game apa'])
+        return jsonify(message="dirumah lah kocak",data=body['lu suka game apa'])
+    elif request.method == 'GET':
+        return jsonify(message="dirumah lah kocak")
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
